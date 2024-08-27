a5dc760edb5f0d6f8e70113ce32366839eb9c6f2
Yogita Shete <yogitashete24@gmail.com>
2024-08-27 15:41:26 +0530

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 // import Image from "next/image";
 
+import Banner from "@/components/Banner";
+
 export default function Home() {
   return (
     <>
-      <h1>Welcome to elib client app.</h1>
+      <Banner/>
     </>
   );
 }

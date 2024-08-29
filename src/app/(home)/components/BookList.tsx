e2952a7e169719cb9ed568789e705e8bb5896af2
Yogita Shete <yogitashete24@gmail.com>
2024-08-29 16:32:55 +0530

    add footer

--- src/app/(home)/components/BookList.tsx
+++ src/app/(home)/components/BookList.tsx
@@ -4,7 +4,7 @@ import BookCard from './BookCard';
 
 const BookList = ({ books }: { books: Book[] }) => {
     return (
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-5xl mx-auto">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 max-w-5xl mx-auto mb-7">
             {books.map((book) => (
                 <BookCard key={book._id} book={book} />
             ))}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer className='border-t p-5 text-center'>
+        <span>@ 2024 Guider&#39;s Book all right reserved.</span>
+      </footer>
+   
+  )
+}
+
+export default Footer
